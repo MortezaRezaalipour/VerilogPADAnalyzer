194916ff14aa3dea317b08d9d66aa0d588a4a36e
MorellRAP <rezaalipour.usi@gmail.com>
2024-03-22 15:16:39 +0100

    Uploading my local files onto GitHub

---
+++ VerilogPDA.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from src.arguments import Arguments
+from src.synthesis import Synthesis
+
+
+def VerilogPAD():
+    # take in teh cli arguments
+    args = Arguments.parse()
+    # create a temporary directory for the intermediate files
+    os.makedirs(f'{sys.argv[0]}.temp', exist_ok=True)
+    os.makedirs(f'{sys.argv[0]}.report', exist_ok=True)
+    syn_obj = Synthesis(args.input_path, f'{sys.argv[0]}.temp', f'{sys.argv[0]}.report')
+
+    pass
+
+
+if __name__ == "__main__":
+    VerilogPAD()
