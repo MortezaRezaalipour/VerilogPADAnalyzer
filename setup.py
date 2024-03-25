b191402dc33b547330c7d3c2ade816cc3fe157e4
MorellRAP <rezaalipour.usi@gmail.com>
2024-03-25 22:12:16 +0100

    setup.py file for pypi package is added!

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+
+VERSION = '0.0.1'
+DESCRIPTION = """
+VerilogPADAnalyzer is a Python application designed to analyze and report the
+Power, Area, and Delay (PAD) of Verilog input circuits.
+"""
+
+
+# Setting up
+setup(
+    name="verilog-pad-analyzer",
+    version=VERSION,
+    author="Morteza Rezaalipour (MorellRAP)",
+    author_email="<rezaalipour.usi@gmail.com>",
+    description=DESCRIPTION,
+    url = "https://github.com/MortezaRezaalipour/VerilogPADAnalyzer",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'verilog', 'PAD', 'synthesis', 'analysis', 'circuit', 'EDA', 'simulation', 'hardware', 'design'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
